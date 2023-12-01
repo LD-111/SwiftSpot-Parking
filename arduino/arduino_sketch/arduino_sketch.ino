15008fa8431ba452d5938bd3c84bab4e0cd1a09d
LORENZO IGNACIO DEVIA RUBIO <l.devia02@ufromail.cl>
2023-12-01 13:35:52 -0300

    The Arduino now collects temperature, moisture, the availability of the parking spot and then uploads them to the MQTT broker

--- arduino/arduino_sketch/arduino_sketch.ino
+++ arduino/arduino_sketch/arduino_sketch.ino
@@ -1,21 +1,146 @@
+
+#include <ArduinoMqttClient.h>
+#if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
+  #include <WiFiNINA.h>
+#elif defined(ARDUINO_SAMD_MKR1000)
+  #include <WiFi101.h>
+#elif defined(ARDUINO_ARCH_ESP8266)
+  #include <ESP8266WiFi.h>
+#elif defined(ARDUINO_PORTENTA_H7_M7) || defined(ARDUINO_NICLA_VISION) || defined(ARDUINO_ARCH_ESP32) || defined(ARDUINO_GIGA)
+  #include <WiFi.h>
+#endif
+
+#include <DHT.h>
+#define DHT11PIN 6
+#define DHTTYPE DHT11
+
+#include "arduino_secrets.h"
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;    // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+
+// To connect with SSL/TLS:
+// 1) Change WiFiClient to WiFiSSLClient.
+// 2) Change port value from 1883 to 8883.
+// 3) Change broker value to a server with a known SSL/TLS root certificate 
+//    flashed in the WiFi module.
+
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
+
+const char broker[] = "test.mosquitto.org";
+int        port     = 1883;
+const char availabilityTopic[] = "ICC452-1/swiftspot/spot1/availability";
+const char temperatureTopic[] = "ICC452-1/swiftspot/spot1/temperature";
+const char moistureTopic[] = "ICC452-1/swiftspot/spot1/humidity";
+
 int trigPin = 0;    // Trigger
 int echoPin = 1;    // Echo
 long duration, cm, inches;
 
+DHT dht(DHT11PIN, DHTTYPE);
+float h,tc;
+
+float temperature;
+float moisture;
+bool availability;
+
 void setup() {
-  // initialize serial communication at 9600 bits per second:
+  //Initialize serial and wait for port to open:
   Serial.begin(9600);
-  //Define inputs and outputs
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // attempt to connect to WiFi network:
+  Serial.print("Attempting to connect to WPA SSID: ");
+  Serial.println(ssid);
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    // failed, retry
+    Serial.print(".");
+    delay(5000);
+  }
+
+  Serial.println("You're connected to the network");
+  Serial.println();
+
+  // You can provide a unique client ID, if not set the library uses Arduino-millis()
+  // Each client must have a unique client ID
+  // mqttClient.setId("clientId");
+
+  // You can provide a username and password for authentication
+  // mqttClient.setUsernamePassword("username", "password");
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while (1);
+  }
+
+  Serial.println("You're connected to the MQTT broker!");
+  Serial.println();
+
+  dht.begin();
+
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A1);
-  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  // call poll() regularly to allow the library to send MQTT keep alives which
+  // avoids being disconnected by the broker
+  mqttClient.poll();
+
+  // to avoid having delays in loop, we'll use the strategy from BlinkWithoutDelay
+  // see: File -> Examples -> 02.Digital -> BlinkWithoutDelay for more info
+
+  readTemperature();
+  Serial.print("Temperature: ");
+  Serial.println(tc);
+
+  mqttClient.beginMessage(temperatureTopic);
+  mqttClient.println(tc);
+  mqttClient.endMessage();
+
+  delay(1000);
+
+  readMoisture();
+  Serial.print("Moisture: ");
+  Serial.println(h);
+
+  mqttClient.beginMessage(moistureTopic);
+  mqttClient.println(h);
+  mqttClient.endMessage();
+
+  delay(1000);
+
+  availability = readAvailability();
+  Serial.print("Availability: ");
+  Serial.println(availability);
+
+  mqttClient.beginMessage(availabilityTopic);
+  mqttClient.println(availability);
+  mqttClient.endMessage();
+
+  delay(1000);
+}
+
+void readTemperature(){
+  delay(2000);
+  tc = dht.readTemperature();
+}
+
+void readMoisture(){
+  delay(2000);
+  h = dht.readHumidity();
+  
+}
+
+bool readAvailability(){
   digitalWrite(trigPin, LOW);
   delayMicroseconds(5);
   digitalWrite(trigPin, HIGH);
@@ -29,22 +154,16 @@ void loop() {
 
   // Convert the time into a distance
   cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
-  inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
 
-    Serial.print(inches);
-  Serial.print("in, ");
   Serial.print(cm);
   Serial.print("cm");
   Serial.println();
 
-  delay(250);
+  delay(1000);
 
-  // print out the value you read:
-  //Serial.println(sensorValue);
-  //delay(1);        // delay in between reads for stability
+  if(cm<8.0){
+    return false;
+  }else{
+    return true;
+  }
 }
-
-
-
-
-
